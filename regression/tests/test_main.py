93b601d1069efb81b7b909921e9a2dd86b81687f
juan carlos macias <juancmaciassalvador@gmail.com>
2025-04-29 08:48:45 +0200

    ADD TEST: test_model_predict_range.py

--- regression/tests/test_main.py
+++ regression/tests/test_main.py
@@ -6,7 +6,6 @@ from main import app
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 client = TestClient(app)
-
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
